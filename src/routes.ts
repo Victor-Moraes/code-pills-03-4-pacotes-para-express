3164694e90d30b400e364f475c89467456f1cc90
VIMGAS <victormoraesgs@gmail.com>
2020-06-05 17:30:04 -0300

    Add express-async-errors package

--- src/routes.ts
+++ src/routes.ts
@@ -6,8 +6,19 @@ const todoController = new TodoController();
 
 const routes = Router();
 
-routes.get('/todos', todoController.index);
-routes.post('/todos', todoController.store);
+routes.get('/todos', (request, response) => {
+  throw new Error('Erro');
+}, todoController.index);
+
+
+const promisedException = async () => new Promise((resolve, reject) => {
+  return reject(new Error('Erro dentro da promise'));
+});
+
+routes.post('/todos', async (request, response) => {
+  return promisedException();
+}, todoController.store);
+
 routes.put('/todos/:id', todoController.update);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'express-async-errors';
 import express from 'express';
 import routes from './routes';
 import helmet from 'helmet';
@@ -6,5 +7,15 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(routes);
+
+app.use((
+    error: Error,
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(error);
+    return response.status(500).json({ msg: error.message });
+});
 
 app.listen(3000);
