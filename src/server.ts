c60681a7b277d9de6d3f5b6205f65c98c7589096
VIMGAS <victormoraesgs@gmail.com>
2020-06-05 15:06:18 -0300

    Add helmet package

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import routes from './routes';
+import helmet from 'helmet';
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 app.use(routes);
 
