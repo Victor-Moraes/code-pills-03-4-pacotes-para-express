daa702b49398b05a905b6b24f24e1e509d422a55
VIMGAS <victormoraesgs@gmail.com>
2020-06-06 08:23:04 -0300

    Add compression package

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import 'express-async-errors';
 import express from 'express';
 import routes from './routes';
 import helmet from 'helmet';
+import compression from 'compression';
 
 const app = express();
+app.use(compression());
 app.use(helmet());
 app.use(express.json());
 app.use(routes);
